e204ec4d722f43bfef9fb31b55b0c55a882d613d
Pr00sty <carlsberg.pawel@wp.pl>
2016-09-02 22:56:20 +0200

    modified:   .gitignore 	modified:   mysite/settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -10,6 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
+#1st comment
+
+
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
